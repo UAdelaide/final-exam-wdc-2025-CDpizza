c4acb19efd567b2646f698826d6efb579b8abcc0
CDpizza <zhaozirong1@gmail.com>
2025-06-20 07:34:57 +0000

    [automatic commit] 2025-06-20 07:34:57 UTC

--- part2/models/db.js
+++ part2/models/db.js
@@ -9,5 +9,15 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
+const response = await fetch('http://localhost:3001/login', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ username: this.username, password: this.password })
+});
 
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:8080', // or '*'
+  credentials: true
+}));
 module.exports = pool;
