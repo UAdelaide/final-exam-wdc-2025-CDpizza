c57d6d8b648523649ccb04434f903de0d9b87c02
CDpizza <zhaozirong1@gmail.com>
2025-06-20 11:34:09 +0000

    [automatic commit] 2025-06-20 11:34:09 UTC

--- part1/Node.js/Express/app/app.js
+++ part1/Node.js/Express/app/app.js
@@ -9,6 +9,14 @@ var dogRouter = require('./routes/api/dog');
 
 var app = express();
 
+// enable cors for cross-origin requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
