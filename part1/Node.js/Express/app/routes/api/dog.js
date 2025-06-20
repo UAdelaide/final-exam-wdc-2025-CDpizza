626dab0836dc3919b3ef595057156bcc1b3e085b
CDpizza <zhaozirong1@gmail.com>
2025-06-20 11:40:27 +0000

    [automatic commit] 2025-06-20 11:40:27 UTC

--- part1/Node.js/Express/app/routes/api/dog.js
+++ part1/Node.js/Express/app/routes/api/dog.js
@@ -1,37 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql2/promise');
-
-// database connection
-let db;
-
-(async () => {
-  try {
-    db = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'DogWalkService'
-    });
-  } catch (err) {
-    console.error('error connecting to dogwalkservice database:', err);
-  }
-})();
 
 router.get('/api/dogs', async (req, res) => {
     try {
       const [rows] = await db.query(`
         SELECT
-          Dogs.dog_id,
-          Dogs.name,
+          Dogs.name AS dog_name,
           Dogs.size,
-          Dogs.owner_id
+          Users.username AS owner_username
         FROM Dogs
+        JOIN Users ON Dogs.owner_id = Users.user_id
       `);
       res.json(rows);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   });
-
-module.exports = router;
