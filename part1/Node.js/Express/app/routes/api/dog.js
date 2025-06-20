b8c71601b225fa06594516f33d4038db981c1c5c
CDpizza <zhaozirong1@gmail.com>
2025-06-20 11:03:24 +0000

    [automatic commit] 2025-06-20 11:03:24 UTC ;; communications error to 208.67.222.222#53: timed out

--- part1/Node.js/Express/app/routes/api/dog.js
+++ part1/Node.js/Express/app/routes/api/dog.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/api/dogs', async (req, res) => {
-    try {
-      const [rows] = await db.query(`
-        SELECT
-          Dogs.name AS dog_name,
-          Dogs.size,
-          Users.username AS owner_username
-        FROM Dogs
-        JOIN Users ON Dogs.owner_id = Users.user_id
-      `);
-      res.json(rows);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
+  try {
+    const [rows] = await db.query(`
+      SELECT
+        Dogs.dog_id,
+        Dogs.name,
+        Dogs.size,
+        Dogs.owner_id
+      FROM Dogs
+    `);
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
